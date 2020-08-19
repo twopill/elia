0ae4a9272787714a9675980d667a6130cfc22438
Elia <ceravolo.elia@gmail.com>
2020-08-19 18:11:26 +0200

    start psp page

--- src/app/component/view/info-project/info-project.component.ts
+++ src/app/component/view/info-project/info-project.component.ts
@@ -1,49 +1,50 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
-import { PageService } from "../../../service/page.service"
+import { ActivatedRoute } from '@angular/router';
+import { PageService } from '../../../service/page.service';
 
 @Component({
   selector: 'app-info-project',
   templateUrl: './info-project.component.html',
-  styleUrls: ['./info-project.component.scss']
+  styleUrls: ['./info-project.component.scss'],
 })
 export class InfoProjectComponent implements OnInit {
-
-  text: string = '';
+  text: string;
   title: string = '';
-  
-  show:boolean = false;
+  img: string = '../../../../assets/pi_svg.svg';
+  show: boolean = false;
 
-  animation: string ='';
+  animation: string = '';
   animation_rect: string = '2s forwards openRectangle ease-in';
 
   constructor(
     private route: ActivatedRoute,
     private pageService: PageService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     const title = this.route.snapshot.paramMap.get('name');
-    
+
     this.text = this.pageService.getPage(id);
+    this.img = this.pageService.getImg(id);
     this.title = this.pageService.getTitle(title);
   }
 
-  header_var = false;
-  @HostListener("document:scroll")
-
-  scrollFunction(){
-    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
-      this.header_var = true;
-      this.animation = '2s forwards closeImg ease-out'
-      this.animation_rect = '2s forwards closeRectangle ease-out'
-    }else 
-      {
-        this.header_var = false;
-        this.animation = '10s infinite loopImgRasp'
-        this.animation_rect = '2s forwards openRectangle ease-in'
-      }  
+  getUrl() {
+    return this.img.toString();
   }
 
+  header_var = false;
+  @HostListener('document:scroll')
+  scrollFunction() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.header_var = true;
+      this.animation = '2s forwards closeImg ease-out';
+      this.animation_rect = '2s forwards closeRectangle ease-out';
+    } else {
+      this.header_var = false;
+      this.animation = '10s infinite loopImgRasp';
+      this.animation_rect = '2s forwards openRectangle ease-in';
+    }
+  }
 }
--- src/app/service/page.service.ts
+++ src/app/service/page.service.ts
@@ -1,28 +1,50 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PageService {
-
   //raspPage = require("./mock-data/rasp-page.json");
 
   public text: string[] = [
     '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
-  ]
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+  ];
 
-  constructor() { }
+  public img: string[] = ['pi_svg.svg', 'psp.svg'];
 
-  getPage(id){
-    for(let i = 0; i <= this.text.length;i++){
-      if(id == i) return this.text[i]
-      else console.log('## testo non trovato ##')
+  public text_obj: any[] = [
+    {
+      id: '0',
+      text:
+        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+      img: '../../../../assets/pi_svg.svg',
+    },
+    {
+      id: '1',
+      text:
+        '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+      img: '../../../../assets/psp.svg',
+    },
+  ];
+
+  constructor() {}
+
+  getPage(id) {
+    for (let i = 0; i <= this.text.length; i++) {
+      if (id == i) return this.text[i];
+      else console.log('## testo non trovato ##');
     }
   }
 
-  getTitle(title){
-    return title;
+  getImg(id) {
+    for (let i = 0; i <= this.img.length; i++) {
+      if (id == i) return this.img[i];
+      else console.log('## img non trovato ##');
+    }
   }
 
+  getTitle(title) {
+    return title;
+  }
 }
