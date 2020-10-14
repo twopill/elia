7293081d478bac3edf79ccd5b1c35ec66fecdf0f
twopill <ceravolo.elia@gmail.com>
2020-10-14 23:26:09 +0200

    first commit new site

--- src/app/component/view/home/home.component.ts
+++ src/app/component/view/home/home.component.ts
@@ -9,46 +9,11 @@ import { faAdjust } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent {
 
-  faCodeBranch = faCodeBranch;
-  faAdjust = faAdjust;
-  faAngry = faAngry;
-
-  name: string;
-
-  experience = [{
-    lenguages: [
-      {
-        "name": 'html',
-        "percentage": 80,
-        "bg": '62868C',
-      },
-      {
-        "name": 'typescript',
-        "percentage": 30,
-        "bg": '62868C',
-      },
-      {
-        "name": 'scss',
-        "percentage": 60,
-        "bg": '62868C',
-      }
-      ,
-      {
-        "name": 'angular',
-        "percentage": 40,
-        "bg": '62868C',
-      }
-    ]
-  }]
-
   constructor() { }
 
   ngOnInit() {
     AOS.init();
   }
 
-  sendName() {
-    // TO DO: SCROLL TO HI PAGE
-  }
 
 }
